9ddb9c1a41956ea385f71ffc6b955b3393cfdc69
Nicola Boyle <nicola162@gmail.com>
2023-03-16 13:32:27 +0000

    added more files to file structure and some pseudocode to JS file

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,24 @@
 // API key for OpenWeatherMap
 
 const APIKey = "aec9049ed82d9e41abe95c26c0050c22"
+
+// Store input value
+
+// Set a function for current location
+
+// Display info
+
+// Function for upcoming forecast
+
+// For loop
+
+// Display info again
+
+// Add event listener
+
+// User local storage to allow user to access search history
+
+// Current weather and future forecast for searched location
+
+// Previous location or search can be accessed on page reload
+
