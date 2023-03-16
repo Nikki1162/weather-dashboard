5ac437fc6d67bfc4a49f1594e70331dbd86bfa90
Nicola Boyle <nicola162@gmail.com>
2023-03-16 17:35:49 +0000

    linked stylesheet, added some styling and some JS logic (couple of fuctions and variables)

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,44 @@
 const APIKey = "aec9049ed82d9e41abe95c26c0050c22"
 
 // Store input value
+let today = moment().format("(DD/MM/YY)");
+let previousSearchHistory = [];
 
-// Set a function for current location
+// Set a function for current location and weather
+function atPresent(city){
+
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`;
+  
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(presentReturn){
+      console.log(presentReturn);
+          
+      $("#weather-result").css("display", "block");
+      $("#location-item").empty(); //remove all child nodes of the set of matched elements from the DOM
+          
+      let weatherSymbol = presentReturn.weather[0].icon;
+      let symbolURL = `https://openweathermap.org/img/w/${weatherSymbol}.png`;
 
 // Display info
+let presentLocation = $(`
+<h4 id="present-location">
+    ${presentReturn.name} ${today} //image here// </h4>
+    <div>Temp: ${presentReturn.main.temp} °c</div>
+    <div>Wind: ${presentReturn.wind.speed} mph</div>
+    `);
+
+    let lat = presentReturn.coord.lat;
+    let lon = presentReturn.coord.lon;
+
+    $("#location-item").append(presentLocation);
+    inFuture(lat, lon);
+    });
+}
 
 // Function for upcoming forecast
+function inFuture(lat, lon);
 
 // For loop
 
